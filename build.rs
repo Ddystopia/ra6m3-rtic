c0ce48aaa9420fcbba2e506791cf58fb691be923
Oleksandr Babak <alexanderbabak@proton.me>
2025-10-26 16:32:52 +0100

    chore: remove qemu build script stuff

--- build.rs
+++ build.rs
@@ -12,9 +12,7 @@ fn main() {
     println!("cargo:rerun-if-changed=script");
     println!("cargo:rustc-link-search=script");
 
-    if cfg!(feature = "qemu") {
-        std::fs::copy("lm3s6965_memory.x", memory_x).unwrap();
-    } else if cfg!(feature = "ra6m3") {
+    if cfg!(feature = "ra6m3") {
         std::fs::copy("ra6m3_memory.x", memory_x).unwrap();
     } else {
         panic!("No memory layout specified. Use --features lm3s6965 or ra6m3.");
