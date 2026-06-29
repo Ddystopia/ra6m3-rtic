284517a0099ec6d86d8fdb9fc94d0876354f3d48
Oleksandr Babak <alexanderbabak@proton.me>
2026-06-29 18:10:05 +0200

    chore: make jlinkgdbserver.sh accept args

--- tools/jlinkgdbserver.sh
+++ tools/jlinkgdbserver.sh
@@ -1,4 +1,8 @@
 #! /bin/sh
 
 jgdbs=$(which JLinkGDBServer)
-$jgdbs -if swd -port 3333 -RTTTelnetPort 3334 -device R7FA6M3AH -jlinkscriptfile ./rtt.JLinkScript
+
+port=${1:-3333}
+rtt_port=${2:-3334}
+
+exec "$jgdbs" -if swd -port "$port" -RTTTelnetPort "$rtt_port" -device R7FA6M3AH -jlinkscriptfile ./rtt.JLinkScript
